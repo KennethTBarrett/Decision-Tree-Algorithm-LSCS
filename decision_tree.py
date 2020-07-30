4ec59d1071ac541850c45b448d5b87ee7763656d
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-07-30 13:08:42 -0500

    PEP8

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Node:
     def __init__(self, X, y):
         # X is our features
@@ -10,36 +11,44 @@ class Node:
         self.column = None
         self.split_point = None
         self.children = None
-    
+
     def is_data_pure(self):
-        # We're going to determine purity of our data based upon probabilities.
-        # If there's a 100% distribution, the data is pure. If there's not, it's
+        # We're going to determine purity of our data based upon .probs().
+        # If there's a 100% distribution, the data is pure.
+        # If there's not, the data is impure.
         p = self.probs()
         if p[0] == 1 or p[1] == 1:
             return True
         return False
-    
 
     def split(self, depth=0):
-        """Makes splits for each column in our features, creates child nodes; recursive until max_depth has been reached."""
+        """Make splits, determining the left and right child nodes, until
+        maximum depth has been reached, or no more splits can be made."""
         # Make our split
         X, y = self.X, self.y
         # Check for leaf status and purity.
-        if self.is_leaf and self.is_data_pure():
-            # Find our best splits based upon the features, target, and column for every column in our features.
-            splits = [self.find_best_split(X, y, column) for column in range(X.shape[1]) ]
-            splits.sort()  # Sort the list we made with list comprehension
+        if self.is_leaf and self.is_data_pure() is False:
+            # Find the best splits to make and store in list.
+            splits = ([self.find_best_split(X, y, column)
+                       for column in range(X.shape[1])])
+            # Sort the list we made with list comprehension.
+            splits.sort()
 
-            gini, split_point, column = splits[0] # Define our gini impurity, split point, and column selected.
-            self.is_leaf = False  # Since we're making a new split, we're gonna need to make sure it's not a leaf.
+            # Fetch information returned from find_best_split()
+            best_split_gini, split_point, column = splits[0]
+
+            # Because new split is being made, not a leaf.
+            self.is_leaf = False
 
             # Define column and split_point
-            self.column = column 
+            self.column = column
             self.split_point = split_point
-            
-            left = X[:,column] <= split_point  # Left if X split at ,column is less than or equal to our split point.
-            right = X[:,column] > split_point   # Right if greater than split point.
-            
+
+            # Left if less than or equal to split point.
+            # Right if greater than split point.
+            left = X[:, column] <= split_point
+            right = X[:, column] > split_point
+
             # We need to define new children nodes for these values.
             self.children = [
                 Node(X[left], y[left]),
@@ -48,8 +57,10 @@ class Node:
             # If we still have depth available
             if depth:
                 for child in self.children:
-                    child.split(depth-1)  # Recursively split, subtracting 1 to reduce our available depth.
-    
+                    # Make split for each new child node,
+                    # and subtract 1 from the available depth.
+                    child.split(depth-1)
+
     def find_best_split(self, X, y, column):
         """Finds best available split based upon Gini"""
         # If we sort our target vector by the values of X,
@@ -59,14 +70,16 @@ class Node:
         self.column = column
 
         # Order our values for ease.
-        order = np.argsort(X[:,column])
+        order = np.argsort(X[:, column])
         classes = y[order]
 
-        # How many of each class are present to the left of our candidate split point?
-        class_0_left = (classes == 0).cumsum()  # Cumsum computes cumulative sum of arrays over axis.
+        # How many of each class are present to the left of split point?
+        # NOTE: .cumsum() computes cumulative sum of arrays over axis.
+        class_0_left = (classes == 0).cumsum()
         class_1_left = (classes == 1).cumsum()
 
-        # Subtracting the cumulative sum from our total will give us our reverse cumulative sum.
+        # Subtracting the cumulative sum from our total will
+        # give us our reverse cumulative sum.
         class_0_right = class_0_left[-1] - class_0_left
         class_1_right = class_1_left[-1] - class_1_left
 
@@ -74,20 +87,34 @@ class Node:
         left_total = class_0_left + class_1_left
         right_total = class_0_right + class_1_right
 
-        np.seterr(divide='ignore', invalid='ignore')  # Ignore Divide by Zero Warning (Let it just Continue)
+        # Ignore Divide by Zero Warning (Let it just Continue)
+        np.seterr(divide='ignore', invalid='ignore')
 
         # Calculate Gini
-        gini = (class_1_left / left_total) * (class_0_left / left_total) + (class_1_right / right_total) * (class_0_right / right_total)
+        gini = ((class_1_left / left_total) * (class_0_left / left_total) +
+                (class_1_right / right_total) * (class_0_right / right_total))
+
+        # Deal with NaN values.
         gini[np.isnan(gini)] = 1
 
-        # Reverse the sorting to make sure we follow the rule
-        # C < split_value
-        best_split_gini = gini[np.argmin(gini)]  # Define our best split based upon minimum gini
-        best_split_index = np.argwhere(order == np.argmin(gini)).item(0)  # Define our best split's index.
-        best_split = X[best_split_index, column]  # Store best split based upon the index and column.
-
+        # Minimum Gini is our best split
+        best_split_rank = np.argmin(gini)
+        # Define the best split in gini.
+        best_split_gini = gini[best_split_rank]
+        # The index of our best split; reverse sorting done above.
+        best_split_index = np.argwhere(order == best_split_rank).item(0)
+        # Use the information to define our best split.
+        best_split = X[best_split_index, column]
         return best_split_gini, best_split, column
 
+    def predict_prob(self, row):
+        if self.is_leaf:
+            return self.probs()  # If it's a leaf, send to probabilities.
+        else:  # If it's not a leaf...
+            if row[self.column] <= self.split_point:  # If <= split point...
+                return self.children[0].predict_prob(row)  # Left child.
+            else:  # If it isn't (> split point)...
+                return self.children[1].predict_prob(row)  # Right child.
 
     def probs(self):
         # We're gonna determine our probabilities using the mean.
@@ -96,33 +123,25 @@ class Node:
             np.mean(self.y == 1),
         ])
 
-    def predict_prob(self, row):
-        if self.is_leaf:
-            return self.probs()  # If it's a leaf, send to probabilities.
-        else:  # If it's not a leaf...
-            if row[self.column] <= self.split_point:  # Check if our column <= our split point.
-                return self.children[0].predict_prob(row)  # If it is, left child.
-            else:  # If it isn't...
-                return self.children[1].predict_prob(row)  # Right child.
 
 class Classifier:
-    def __init__(self, max_depth=3):
+    def __init__(self, max_depth=5):
         self.max_depth = int(max_depth)
         self.root = None
-        
+
     def fit(self, X, y):
         """Usage: clf.fit(X_train, y_train)
-        Fit model to training data; learn relationship between features X and target y"""
-        self.root = Node(X, y)  # Define our root node from which to begin the splitting process.
-        self.root.split(self.max_depth)  # Split with the selected maximum depth.
+        Fit model to training data; learns relationship
+        between features X and target y"""
+        self.root = Node(X, y)  # Define our tree's root node.
+        self.root.split(self.max_depth)  # Begin splitting process.
 
     def predict(self, X):
         """Usage: clf.fit(X_test)
-        Make predictions on testing features, based upon what the model has learned about
-        the data from fitting."""
+        Make predictions on testing features, based upon what the model
+        has learned about the data during fitting."""
         results = []
         for row in X:  # For every row in our feature,
-            p = self.root.predict_prob(row)  # Recursive methodology to predict proba
-            results += [p]  # Add the proba to our results.
-        results = np.array(results)
-        return (results[:,1] > 0.5).astype(int)  # Return predictions.
+            p = self.root.predict_prob(row)  # Predict probabilities.
+            results += [p]  # Add to our results.
+        return (np.array(results)[:, 1] > 0.5).astype(int)  # Prediction.
