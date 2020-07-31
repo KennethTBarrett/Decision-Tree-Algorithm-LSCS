bf0e4e8ca5f54731833ede78056156590900405a
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-07-31 01:56:13 -0500

    Major overhaul (finally figured out my improvements)

--- decision_tree.py
+++ decision_tree.py
@@ -1,147 +1,133 @@
 import numpy as np
+from collections import Counter
 
 
 class Node:
-    def __init__(self, X, y):
-        # X is our features
-        # y is our target
-        self.X = X
-        self.y = y
-        self.is_leaf = True
-        self.column = None
-        self.split_point = None
-        self.children = None
+    def __init__(self, left=None, right=None, best_feature=None, threshold=None, value=None):
+        self.left = left
+        self.right = right
+        self.best_feature = best_feature
+        self.threshold = threshold
+        self.value = value
+    
+    def is_node_leaf(self):
+        """Determines if node is a leaf node."""
+        return self.value is not None
 
-    def is_data_pure(self):
-        # We're going to determine purity of our data based upon .probs().
-        # If there's a 100% distribution, the data is pure.
-        # If there's not, the data is impure.
-        p = self.probs()
-        if p[0] == 1 or p[1] == 1:
-            return True
-        return False
-
-    def split(self, depth=0):
-        """Make splits, determining the left and right child nodes, until
-        maximum depth has been reached, or no more splits can be made."""
-        # Make our split
-        X, y = self.X, self.y
-        # Check for leaf status and purity.
-        if self.is_leaf and self.is_data_pure() is False:
-            # Find the best splits to make and store in list.
-            splits = ([self.find_best_split(X, y, column)
-                       for column in range(X.shape[1])])
-            # Sort the list we made with list comprehension.
-            splits.sort()
-
-            # Fetch information returned from find_best_split()
-            best_split_gini, split_point, column = splits[0]
-
-            # Because new split is being made, not a leaf.
-            self.is_leaf = False
-
-            # Define column and split_point
-            self.column = column
-            self.split_point = split_point
-
-            # Left if less than or equal to split point.
-            # Right if greater than split point.
-            left = X[:, column] <= split_point
-            right = X[:, column] > split_point
-
-            # We need to define new children nodes for these values.
-            self.children = [
-                Node(X[left], y[left]),
-                Node(X[right], y[right])
-            ]
-            # If we still have depth available
-            if depth:
-                for child in self.children:
-                    # Make split for each new child node,
-                    # and subtract 1 from the available depth.
-                    child.split(depth-1)
-
-    def find_best_split(self, X, y, column):
-        """Finds best available split based upon Gini"""
-        # If we sort our target vector by the values of X,
-        # counting classes to go left / right of our split point will be easy.
-        self.X = X
-        self.y = y
-        self.column = column
-
-        # Order our values for ease.
-        order = np.argsort(X[:, column])
-        classes = y[order]
-
-        # How many of each class are present to the left of split point?
-        # NOTE: .cumsum() computes cumulative sum of arrays over axis.
-        class_0_left = (classes == 0).cumsum()
-        class_1_left = (classes == 1).cumsum()
-
-        # Subtracting the cumulative sum from our total will
-        # give us our reverse cumulative sum.
-        class_0_right = class_0_left[-1] - class_0_left
-        class_1_right = class_1_left[-1] - class_1_left
-
-        # Calculate the totals for left and right.
-        left_total = class_0_left + class_1_left
-        right_total = class_0_right + class_1_right
-
-        # Ignore Divide by Zero Warning (Let it just Continue)
-        np.seterr(divide='ignore', invalid='ignore')
-
-        # Calculate Gini
-        gini = ((class_1_left / left_total) * (class_0_left / left_total) +
-                (class_1_right / right_total) * (class_0_right / right_total))
-
-        # Deal with NaN values.
-        gini[np.isnan(gini)] = 1
-
-        # Minimum Gini is our best split
-        best_split_rank = np.argmin(gini)
-        # Define the best split in gini.
-        best_split_gini = gini[best_split_rank]
-        # The index of our best split; reverse sorting done above.
-        best_split_index = np.argwhere(order == best_split_rank).item(0)
-        # Use the information to define our best split.
-        best_split = X[best_split_index, column]
-        return best_split_gini, best_split, column
-
-    def predict_prob(self, row):
-        if self.is_leaf:
-            return self.probs()  # If it's a leaf, send to probabilities.
-        else:  # If it's not a leaf...
-            if row[self.column] <= self.split_point:  # If <= split point...
-                return self.children[0].predict_prob(row)  # Left child.
-            else:  # If it isn't (> split point)...
-                return self.children[1].predict_prob(row)  # Right child.
-
-    def probs(self):
-        # We're gonna determine our probabilities using the mean.
-        return np.array([
-            np.mean(self.y == 0),
-            np.mean(self.y == 1),
-        ])
-
-
+    
 class Classifier:
     def __init__(self, max_depth=5):
-        self.max_depth = int(max_depth)
         self.root = None
+        self.max_depth = max_depth
 
     def fit(self, X, y):
-        """Usage: clf.fit(X_train, y_train)
-        Fit model to training data; learns relationship
-        between features X and target y"""
-        self.root = Node(X, y)  # Define our tree's root node.
-        self.root.split(self.max_depth)  # Begin splitting process.
-
+        self.root = self._make_split(X, y)
+        return self
+    
     def predict(self, X):
-        """Usage: clf.fit(X_test)
-        Make predictions on testing features, based upon what the model
-        has learned about the data during fitting."""
-        results = []
-        for row in X:  # For every row in our feature,
-            p = self.root.predict_prob(row)  # Predict probabilities.
-            results += [p]  # Add to our results.
-        return (np.array(results)[:, 1] > 0.5).astype(int)  # Prediction.
+        """Makes prediction by traversing down our decision tree."""
+        return np.array([self._make_prediction(feature, self.root) for feature in X])
+
+    def _make_split(self, X, y, depth=0):
+        # Set our number of samples and features according to feature input shape.
+        num_features = X.shape[1]
+        num_labels = len(np.unique(y))  # How many unique labels?
+        # Check if base case is met:
+        # Maximum depth has been reached or only one label
+        if (depth >= self.max_depth
+                or num_labels == 1):
+            leaf_value = self._most_common(y)
+            return Node(value=leaf_value)  # Create leaf node if so.
+        
+        # Select the indices of a few random features to be used in information gain.
+        random_features = np.random.choice(num_features, num_features, replace=False)
+
+        # Use greedy search to find our best available split, according to information gain calculations.
+        best_feature, threshold = self._find_best(X, y, random_features)
+
+        # Determine left and right indices (left <= threshold, and right > threshold).
+        left_idx, right_idx = self._what_splits(X[: ,best_feature], threshold)
+        
+        # Recursively make splits until base case is met, adding 1 to the depth each time.
+        left = self._make_split(X[left_idx, :], y[left_idx], depth+1)
+        right = self._make_split(X[right_idx, :], y[right_idx], depth+1)
+
+        return Node(left, right, best_feature, threshold)
+
+    def _find_best(self, X, y, feature_indices):
+        # Define default values for best gain, index, and threshold.
+        best_gain = -1
+        split_index = None
+        split_threshold = None
+        # Iterate through list of feature indices.
+        for index in feature_indices:
+            # Set the selected column
+            X_column = X[:, index]
+            thresholds = np.unique(X_column)  # Determine unique values.
+            for threshold in thresholds:
+                # Iterate through and calculate the information gain.
+                gain = self._calculate_information_gain(X_column, y, threshold)
+                
+                # If the gain we calculated is better than the previous best gain
+                # we're going to use it to set our split index and threshold, as
+                # well as re-define the current best gain for our next pass.
+                if gain > best_gain:
+                    best_gain = gain
+                    split_index = index
+                    split_threshold = threshold
+        return split_index, split_threshold
+
+    def _what_splits(self, X, split_threshold):
+        # Split should be left if X <= our threshold.
+        left = np.argwhere(X <= split_threshold).flatten()
+        # Split should be right if X > our threshold.
+        right = np.argwhere(X > split_threshold).flatten()
+        return left, right
+
+    def _calculate_entropy(self, y):
+        histo = np.bincount(y)  # Counts occurence of each element
+        ps = histo / len(y)
+        # List comprehension; calculate entropy.
+        return -np.sum([p * np.log2(p) for p in ps if p > 0])
+
+    
+    def _calculate_information_gain(self, X, y, split_threshold):
+        # Calculate entropy of the parent
+        parent_entropy = self._calculate_entropy(y)
+        # Determine the left and right values.
+        left, right = self._what_splits(X, split_threshold)
+
+        # Confirm there's information gain.
+        if len(left) == 0 or len(right) == 0:
+            return 0
+
+        # Calculate the weighted average of child entropy
+        num = len(y)
+        num_left, num_right = len(left), len(right)
+        # Calculate the entropy for the left side, as well as the right.
+        entropy_left, entropy_right = self._calculate_entropy(y[left]), self._calculate_entropy(y[right])
+
+        # Determine the entropy of the children, using the formula:
+        # (length of the values on the left / total number of values) * left side entropy +
+        # (length of the values on the right / total number of values) * right side entropy
+        child_entropy = (num_left/num) * entropy_left + (num_right/num) * entropy_right
+
+        # Return information gain by subtracting the child entropy from the parent entropy.
+        information_gain = parent_entropy - child_entropy
+        return information_gain
+
+
+    def _most_common(self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
+
+    def _make_prediction(self, X, node):
+        # Check if the node is a leaf. If it is, return its value.
+        if node.is_node_leaf():
+            return node.value
+        # Otherwise, traverse down the decision tree.
+        if X[node.best_feature] <= node.threshold:
+            return self._make_prediction(X, node.left)
+        else:
+            return self._make_prediction(X, node.right)
